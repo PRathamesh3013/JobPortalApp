82d6a41fb3180573f3df1d56bb24344f5479f70e
Prathamesh Naik <prathameshnaik902@gmail.com>
2024-02-06 17:41:05 +0530

    first Sprint completed

---
+++ src/main/java/com/jobportal/JobPortal/Implements/JobImplements.java
@@ -0,0 +1,50 @@
+package com.jobportal.JobPortal.Implements;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jobportal.JobPortal.Model.Jobs;
+import com.jobportal.JobPortal.Repository.JobRepository;
+import com.jobportal.JobPortal.Services.JobServices;
+@Service
+public class JobImplements implements JobServices{
+	@Autowired
+	JobRepository jobRepo;
+	
+	@Override
+	public Jobs createJobs(Jobs user) {
+		// TODO Auto-generated method stub
+		return jobRepo.save(user);
+	}
+	public List<Jobs> getJobs() {
+		// TODO Auto-generated method stub
+		return jobRepo.findAll();
+	}
+	@Override
+	public Jobs getById(int id) {
+		// TODO Auto-generated method stub		
+		return jobRepo.findById(id).get();
+	}
+	@Override
+	public Jobs updateJobs(Jobs jobs, int id) {
+		// TODO Auto-generated method stub
+		Jobs updatedJob= jobRepo.findById(id).get();
+		if(updatedJob!=null) {
+			updatedJob.setId(id);
+			updatedJob.setCompanyName(jobs.getCompanyName());
+			updatedJob.setJobDesc(jobs.getJobDesc());
+			updatedJob.setDeadline(jobs.getDeadline());
+			return jobRepo.save(updatedJob);
+		}
+		else return updatedJob;
+	}
+	@Override
+	public void deleteJobs(int id) {
+		jobRepo.deleteById(id);
+	}
+	
+	
+	
+}
